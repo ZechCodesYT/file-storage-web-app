4acf23a3141d5f6880caa2a85156fc8c01b53012
ZechCodes <hi@zech.codes>
2022-05-01 17:45:12 -0400

    Add login route

--- server/api.py
+++ server/api.py
@@ -4,7 +4,7 @@ import logging
 from server.database import db
 from server.database.models.users import User
 from server.database.models import BaseModel
-from server.auth import hash_password
+from server.auth import hash_password, check_password_matches
 
 
 API_app = fastapi.FastAPI()
@@ -20,4 +20,14 @@ class PartialUser(BaseModel):
 async def register_user(user: PartialUser, conn=fastapi.Depends(db)):
     await User.create(user.name, hash_password(user.password), conn)
     user = await User.get_by("name", user.name, conn)
+    await conn.commit()
     return {"message": "SUCCESS", "user-id": user[0].id}
+
+
+@API_app.post("/login")
+async def login_user(user_info: PartialUser, conn=fastapi.Depends(db)):
+    user = await User.get_by("name", user_info.name, conn)
+    if not user or not check_password_matches(user_info.password, user[0].password):
+        return {"status": "FAILED", "message": f"User's name or password was incorrect. {user!r}"}
+
+    return {"status": "SUCCESS", "user-id": user[0].id}
