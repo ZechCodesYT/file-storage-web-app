19f572f9d989ea2679dbfc4b85143ab34d7529ea
ZechCodes <hi@zech.codes>
2022-06-12 18:02:29 -0400

    Create folders on the server that exist locally

--- cli/sync.py
+++ cli/sync.py
@@ -12,12 +12,28 @@ def sync_folder(folder_name: str, local_folder: pathlib.Path):
         typer.secho(f"Folder {folder_name} not found, creating.")
         folder = _create_folder(folder_name, -1)
 
+    folder = asyncio.run(APIWrapper().get_folder(folder.id))
     typer.secho(f"Syncing {local_folder} to {folder_name}", fg=typer.colors.GREEN)
+    asyncio.run(_do_folder_sync(local_folder, folder))
 
 
 def _create_folder(name: str, parent_id: int) -> Folder:
     api = APIWrapper()
     return asyncio.run(api.create_folder(name, parent_id))
+
+
+async def _do_folder_sync(local_folder: pathlib.Path, folder: Folder, api: APIWrapper | None = None):
+    api = api or APIWrapper()
+    new_folders = []
+    for f in local_folder.iterdir():
+        if f.is_dir() and not _find_folder_in_folder_by_name(f.name, folder) and not f.name.startswith((".", "_")):
+            new_folders.append(await api.create_folder(f.name, folder.id))
+
+    # Todo: Upload missing files to the server
+    # Todo: Recursively upload files in sub-folders
+    # Todo: Ignore files that haven't changed
+    # Todo: Download missing folders recursively
+    # Todo: Download missing files
 
 
 def _find_folder_in_folder_by_name(name: str, folder: Folder) -> Folder | None:
